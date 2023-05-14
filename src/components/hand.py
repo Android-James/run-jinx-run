c390a59a6843d805d9e853eed6be6f78f47de9cb
blancojames88 <blancojames88@gmail.com>
2023-05-15 01:59:02 +0800

    update hand.py

--- src/components/hand.py
+++ src/components/hand.py
@@ -2,12 +2,10 @@ import random
 
 import pygame
 
-from src.components.hand_side import HandSide
 from src.components.scoreboard import Scoreboard
 from src.config import Config
 from src.services.music_service import MusicService
 from src.services.visualization_service import VisualizationService
-from src.utils.tools import sine
 
 
 class Hand(pygame.sprite.Sprite):
