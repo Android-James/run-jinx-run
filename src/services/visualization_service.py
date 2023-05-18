e16eb16dd1ae81e88d4f7ecf4e6ffb3ed63b547b
blancojames88 <blancojames88@gmail.com>
2023-05-18 15:57:48 +0800

    added bullet png

--- src/services/visualization_service.py
+++ src/services/visualization_service.py
@@ -7,12 +7,8 @@ from src.utils.tools import sine
 
 class VisualizationService:
     @staticmethod
-    def get_right_hand_image():
-        return pygame.image.load(ASSETS_DIR / "right_hand.png").convert_alpha()
-
-    @staticmethod
     def get_left_hand_image():
-        return pygame.image.load(ASSETS_DIR / "left_hand.png").convert_alpha()
+        return pygame.image.load(ASSETS_DIR / "bullet.png").convert_alpha()
     
     @staticmethod
     def get_rocket_image():
